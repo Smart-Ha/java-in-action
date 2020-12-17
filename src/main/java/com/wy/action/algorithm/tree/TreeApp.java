38079e3c07a524c18b4a38af180e02a79b6c33ab
wangyong <865469028@qq.com>
2020-12-17 13:53:40 +0800

    level order

--- src/main/java/com/wy/action/algorithm/tree/TreeApp.java
+++ src/main/java/com/wy/action/algorithm/tree/TreeApp.java
@@ -1,6 +1,9 @@
 package com.wy.action.algorithm.tree;
 
 import com.wy.action.entity.TreeNode;
+import org.junit.Test;
+
+import java.util.*;
 
 /**
  * @Author wangyong
@@ -53,5 +56,47 @@ public class TreeApp {
 
         return p.val == q.val && isSymmetricRec(p.left, q.right) && isSymmetricRec(p.right, q.left);
     }
+
+    /**
+     * 给定一个二叉树，返回其节点值的级别顺序遍历。 （即，从左到右，逐级）。
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue  = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> one = new ArrayList<>();
+            for (int i=0; i<size; i++) {
+                TreeNode treeNode = queue.poll();
+                if (treeNode.left != null ) {
+                    queue.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.add(treeNode.right);
+                }
+                one.add(treeNode.val);
+            }
+
+            result.add(one);
+        }
+
+        return result;
+    }
+
+    @Test
+    public void levelOrderTest() {
+        TreeNode treeNode = TreeNode.bfsBuild(Arrays.asList(3,9,20,null,null,15,7));
+        System.out.println(levelOrder(treeNode));
+        treeNode = TreeNode.bfsBuild(Arrays.asList(1,2,null,3,null,4,null,5));
+        System.out.println(levelOrder(treeNode));
+    }
+
 
 }
