48bf2446b8ad26a99e7165faf26cd07a18f9ec9e
Cat <865469028@qq.com>
2021-01-05 22:31:59 +0800

    insert bst

--- src/main/java/com/wy/action/algorithm/tree/BinarySearchTree.java
+++ src/main/java/com/wy/action/algorithm/tree/BinarySearchTree.java
@@ -290,4 +290,17 @@ public class BinarySearchTree {
         convertBST(root.left);
         return root;
     }
+
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
 }
