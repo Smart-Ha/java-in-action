2dc417e5d4fe08fa1548daae844f5010da76d63b
Cat <865469028@qq.com>
2021-01-15 23:39:58 +0800

    removeLeaf

--- src/main/java/com/wy/action/algorithm/tree/BinarySearchTree3.java
+++ src/main/java/com/wy/action/algorithm/tree/BinarySearchTree3.java
@@ -172,4 +172,16 @@ public class BinarySearchTree3 {
         Assert.assertEquals(18, sumEvenGrandparent(node));
 
     }
+
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root == null) {
+            return null;
+        }
+        root.left = removeLeafNodes(root.left,target);
+        root.right = removeLeafNodes(root.right,target);
+        if (root.val == target && root.left == null && root.right == null) {
+            return null;
+        }
+        return root;
+    }
 }
